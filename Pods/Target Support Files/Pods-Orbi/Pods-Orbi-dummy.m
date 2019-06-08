b0198c7de62c8e2d088bac91972ea062b3e6fac1
adibdehghan <adb.dehghan@gmail.com>
2019-06-09 04:23:28 +0430

    pods

---
+++ Pods/Target Support Files/Pods-Orbi/Pods-Orbi-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Orbi : NSObject
+@end
+@implementation PodsDummy_Pods_Orbi
+@end
---
+++ Pods/Target Support Files/Pods-OrbiTests/Pods-OrbiTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OrbiTests : NSObject
+@end
+@implementation PodsDummy_Pods_OrbiTests
+@end
